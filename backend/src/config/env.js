63b296c9c3f37306b04f8aee654653285013c8d2
Your Name <ritiksharma10508@gmail.com>
2025-07-21 15:28:02 +0530

    db models created

---
+++ backend/src/config/env.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export const ENV = {
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  MONGO_URI: process.env.MONGO_URI,
+  CLERK_PUBLICATION_KEY: process.env.CLERK_PUBLICATION_KEY,
+  CLERK_SECRET_KET: process.CLERK_SECRET_KET,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  ARCJET_KEY: process.env.ARCJET_KEY,
+}
